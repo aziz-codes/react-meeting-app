ee11a030633dfea40ae54e72a98bdee8d5c4e247
aziz-codes <azizisoffline@gmail.com>
2024-11-22 18:37:27 +0500

    context and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
  import { Routes,Route } from "react-router-dom"
 import Lobby from "./pages/lobby"
 import Room from "./pages/room"
+import Home from "./pages/home"
 const App = () => {
   return (
      <div>
        <Routes>
-         <Route path="/" element={<Lobby />} />
+         <Route path="/" element={<Home/>} />
+         <Route path="/lobby" element={<Lobby />} />
          <Route path="/room/:id" element={<Room />} />
        </Routes>
      </div>
--- client/src/pages/lobby.tsx
+++ client/src/pages/lobby.tsx
@@ -1,7 +1,12 @@
  
 const Lobby = () => {
   return (
-    <div>Lobby</div>
+    <div className="flex min-h-screen items-center px-0 md:px-20">
+       <div className="flex items-center w-full gap-4">
+        <div className="flex-1 border h-72 rounded-md">video</div>
+        <div className="flex-1 border">buttons</div>
+        </div> 
+    </div>
   )
 }
 
